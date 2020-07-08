cb450d4843a1670d1c8211e5ff04aa8675e35699
Andrey Aleksovski <aleksovski_andrey@hotmail.com>
2020-07-08 10:11:46 +0200

    basic

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,40 @@
 package com.company;
 
+import java.util.ArrayList;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        ArrayList<String> cities = new ArrayList<String>();
+        cities.add("Vienna");
+        cities.add("Graz");
+        cities.add("Linz");
+        cities.add("Salzburg");
+        cities.add("Bregenz");
+        cities.add("Dornbirn");
+
+        for(String i : cities){
+            System.out.println(i);
+        }
+
+        ArrayList<String> temperature = new ArrayList<String>();
+        temperature.add("19");
+        temperature.add("17");
+        temperature.add("11");
+        temperature.add("34");
+        temperature.add("23");
+        temperature.add("7");
+
+        for(String i : temperature){
+            System.out.println(i);
+        }
+
+        cities.addAll(temperature);
+
+        for(String i : cities){
+            System.out.println(i);
+        }
+
     }
 }
